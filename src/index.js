bc89bb4dbf2d05a6ecb27046ed9b2dd83584ceeb
a.Vienik <alexandrVenik@gmail.com>
2021-04-05 20:23:22 +0300

    test commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './styles.css';
 import './js/menu';
 import './js/apartments';
+
+// class User {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+const User = function (name, age) {
+  // [[environment]]: { this: {} }
+  // this = {}
+  this.name = name;
+  // this = { name: name }
+  this.age = age;
+  // this = { name: name, age: age }
+};
+
+const user = new User('John', 20);
+console.log(user);
